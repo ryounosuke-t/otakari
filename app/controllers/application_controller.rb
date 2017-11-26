5aec7aea80d3ee828c9eae14344cff370d6300ac
竹内りょうのすけ <takeuchiryounosuke@takeuchiryounosukes-MacBook-Pro.local>
2017-11-26 21:01:36 +0900

    created timeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :profile, :password, :current_password, :password_confirmation])
   end
