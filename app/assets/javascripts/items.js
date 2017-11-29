2d8e1dd3a71a559b7f85d1e70828cba4436ab5f9
竹内りょうのすけ <takeuchiryounosuke@takeuchiryounosukes-MacBook-Pro.local>
2017-11-29 14:46:46 +0900

    created profile edit page

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+$(window).on("load", function() {
+  $("li").on("click", function() {
+    $("li.selected").removeClass("selected");
+    $(this).addClass("selected");
+    $(".contents div").hide(); // 二つの要素を非表示にする
+    $("." + this.id).show(); // クリックされたボタンに対応する要素を表示する
+  });
+});
