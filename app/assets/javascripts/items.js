84b20dfa620f4a51aafd93cf3be6b05052253433
竹内りょうのすけ <takeuchiryounosuke@takeuchiryounosukes-MacBook-Pro.local>
2017-11-30 21:53:06 +0900

    fixed jquery part

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -7,34 +7,23 @@ $(function(){
   // タブメニューをクリックしたとき
   $('.js-tab > li').click(function(){
 
-    // タブメニューとタブコンテンツのクラス「active」を削除
-    $('.js-tab > li,.js-tab_content').removeClass('selected');
-
-    // タブメニューのクラスを取得し、変数「tabClass」に格納（例：sky）
-    var tabClass = $(this).attr('class');
-    console.log($(this).attr('class').indexOf(tabClass));
-    // クリックしたタブメニューにクラス「active」を付与
+    $('.js-tab > li').removeClass('selected');
     $(this).addClass('selected');
 
-
+  });
     // それぞれのタブコンテンツに対して
-    $('.js-tab_content').each(function(){
+    $('.sky').click(function(){
 
-      // 変数「tabClass」と、同じクラスがついたタブコンテンツに
-      if(tabClass == "sunset") {
-
-        // クラス「active」を付与し、フェードインしながら表示
-        $(this).addClass('selected').show();
-      // それ以外のタブコンテンツは
-      }else{
-
-        // 隠す
-        $(this).hide();
-
-      }
-
-    });
+        $('.left').addClass('selected');
+        $('.right').removeClass('selected');
 
   });
+
+    $('.sunset').click(function(){
+
+      $('.left').removeClass('selected');
+        $('.right').addClass('selected');
+
+    });
 
 });
--- app/assets/javascripts/timelines.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-  $('#sample1-1 .mod-tab-nav').tab();
-});
