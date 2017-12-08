a5bdbea0c574266417dc0bcc8b6d2b3e8af75d56
竹内りょうのすけ <takeuchiryounosuke@takeuchiryounosukes-MacBook-Pro.local>
2017-12-08 13:10:35 +0900

    fixed little bit

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,7 @@
 class TimelinesController < ApplicationController
 
   def index
-    @items=Item.all.limit(4)
+    @items=Item.all.limit(10)
     if signed_in?
       redirect_to all_items_path
     end
